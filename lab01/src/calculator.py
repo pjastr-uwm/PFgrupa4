ac27022890306a81a3f7a239d8056e25c2392539
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2025-02-28 14:40:45 +0100

    zad1

--- lab01/src/calculator.py
+++ lab01/src/calculator.py
@@ -2,3 +2,8 @@ class Calculator:
 
     def add(self, x, y):
         return x + y
+
+    def divide(self, x, y):
+        if y == 0:
+            raise ZeroDivisionError
+        return x / y
--- lab01/tests/test_calculator.py
+++ lab01/tests/test_calculator.py
@@ -13,6 +13,10 @@ class TestCalculator(unittest.TestCase):
     def test_sum_two_ints_negative(self):
         self.assertNotEqual(self.calculator.add(4, 8), 2)
 
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calculator.divide(5, 0)
+
     def tearDown(self):
         pass
 

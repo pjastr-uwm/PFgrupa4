0790962395224c8729da3f774221c532aba2a60a
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 14:02:44 +0100

    etap 4

--- lab03/src/book.py
+++ lab03/src/book.py
@@ -3,3 +3,6 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
+
+    def calculate_reading_time(self):
+        return self.page_count // 2
--- lab03/tests/test_book.py
+++ lab03/tests/test_book.py
@@ -16,6 +16,12 @@ class TestBookInitialization(unittest.TestCase):
         self.assertEqual(book1.title, "Pride and Prejudice")
         self.assertEqual(book1.page_count, 432)
 
+    def test_calculate_reading_time(self):
+        book1 = Book("Pride and Prejudice", 432)
+        self.assertEqual(book1.calculate_reading_time(), 216)
+        book2 = Book("Animal Farm", 112)
+        self.assertEqual(book2.calculate_reading_time(), 56)
+
     def tearDown(self):
         pass
 

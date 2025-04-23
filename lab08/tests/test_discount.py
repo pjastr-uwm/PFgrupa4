7f4351f0b43c8330ffdc286ffa44eca0310a1063
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-04-23 13:52:38 +0200

    Update test_discount.py

--- lab08/tests/test_discount.py
+++ lab08/tests/test_discount.py
@@ -1,4 +1,4 @@
-#import pytest
+import pytest
 from src.discount import calculate_discounted_price
 
 
@@ -8,3 +8,13 @@ def test_calcaule_positive():
 
 def test_calcaule_neg():
     assert calculate_discounted_price(100, 36) != 29
+
+
+def test_invalid_price():
+    with pytest.raises(ValueError):
+        calculate_discounted_price("-4", 89)
+
+def test_invalid_discount():
+    with pytest.raises(ValueError):
+        calculate_discounted_price(100,-78)
+        
